aa7b420f84febdbe4f09792bd5083cb0b2718c58
Liam <liam1540@googlemail.com>
2016-07-29 11:01:46 -0700

    Create card.hpp

---
+++ card.hpp
@@ -0,0 +1,14 @@
+//
+//  card.hpp
+//  blackjack-two
+//
+//  Created by Liam on 29/07/2016.
+//  Copyright © 2016 Liam. All rights reserved.
+//
+
+#ifndef card_hpp
+#define card_hpp
+
+
+
+#endif /* card_hpp */
