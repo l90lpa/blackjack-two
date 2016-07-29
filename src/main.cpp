15e48dcfb0931f3bd04c3e894b96497561a6c741
Liam <liam1540@googlemail.com>
2016-07-29 10:58:50 -0700

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  blackjack-two
+//
+//  Created by Liam on 29/07/2016.
+//  Copyright © 2016 Liam. All rights reserved.
+//
+
+#include <iostream>
+#include "Card.hpp"
+
+int main()
+{
+    return 0;
+}
